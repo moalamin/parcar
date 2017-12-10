c4cffc3d81dff436e72cea6f63ffdfa148c9a88e
Mo Amin <m.alamin92@gmail.com>
2017-12-10 02:44:13 -0500

    fetch location then center map

--- src/screens/LiveMapScreen.js
+++ src/screens/LiveMapScreen.js
@@ -5,27 +5,41 @@ import MapView from "react-native-maps";
 export default class LiveMapScreen extends Component {
   constructor(props) {
     super(props);
-    let lat = null;
-    let long = null;
-    navigator.geolocation.getCurrentPosition((location)=>{
-      console.log(location)
-      lat=location.coords.latitude;
-      long=location.coords.longitude;
-    });
     this.state = {
-      latitude: lat,
-      longitude: long,
+      latitude: 0,
+      longitude: 0,
       latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }
+      longitudeDelta: 0.0421
+    };
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
-
+  componentDidMount(){
+    let that = this;
+    navigator.geolocation.getCurrentPosition(location => {
+      console.log(location)
+      that.setState({
+        region: {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }
+      });
+    });
+  }
+  onRegionChange(region) {
+    this.setState({ region });
+  }
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <MapView 
-          style={{ width: "100%", height: "100%" }} 
-          region={this.state.region}/>
+      <View style={{ flex: 1, justifyContent: 'center'  }}>
+        <MapView
+          style={{ width: "100%", height: "80%"}}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
+          onRegionChange={this.onRegionChange}
+          region={this.state.region}
+        />
       </View>
     );
   }
